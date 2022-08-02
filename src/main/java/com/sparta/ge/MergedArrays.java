2c6f25d90aafeb8186409d6857174ab98f25f85d
Georgiana <georgianaemiliou@gmail.com>
2022-08-02 19:24:22 +0100

    MergedArraysTest Class added

--- src/main/java/com/sparta/ge/MergedArrays.java
+++ src/main/java/com/sparta/ge/MergedArrays.java
@@ -2,7 +2,7 @@ package com.sparta.ge;
 
 import java.util.Arrays;
 
-public class MergeArrays {
+public class MergedArrays {
     public static void main(String[] args) {
         int[] array1 = {1, 2, 3, 4};
         int[] array2 = {5, 6, 7, 8};
