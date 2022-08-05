1a1eab0877ba96e929ebcd587ae23744dad7446a
Georgiana <georgianaemiliou@gmail.com>
2022-08-05 12:34:28 +0100

    bubbleSortMethod added which sorts an array

--- src/main/java/com/sparta/ge/BubbleSort.java
+++ src/main/java/com/sparta/ge/BubbleSort.java
@@ -8,6 +8,7 @@ public class BubbleSort {
         int[] arrayToSort = {3, 7, 1, -8, 0};
         System.out.println("Given array: " + Arrays.toString(arrayToSort));
         System.out.println("Sorted array: " +Arrays.toString(bubbleSort(arrayToSort)));
+        System.out.println("Sorted array: " +Arrays.toString(bubbleSortMethod(arrayToSort)));
     }
 
     static int[] oneIterationSort(int[] arrayToSearch){
@@ -25,6 +26,22 @@ public class BubbleSort {
     static int[] bubbleSort(int[] arrayToSort){
         for(int i=0 ; i<arrayToSort.length-1; i++) {
             arrayToSort = oneIterationSort(arrayToSort);
+        }
+        return arrayToSort;
+    }
+
+    //Bubble sort using one function only
+    static int[] bubbleSortMethod(int[] arrayToSort){
+        int length = arrayToSort.length;
+        int temp;
+        for(int i=0; i<length-1; i++){
+            for(int j=0; j<length-i-1; j++){
+                if(arrayToSort[j] > arrayToSort[j+1]){
+                    temp = arrayToSort[j];
+                    arrayToSort[j] = arrayToSort[j+1];
+                    arrayToSort[j+1] = temp;
+                }
+            }
         }
         return arrayToSort;
     }
